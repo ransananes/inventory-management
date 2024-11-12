a877d17e429d8a3d38d7f480f7968742b3137630
ransananes <51711790+ransananes@users.noreply.github.com>
2024-11-12 21:25:17 +0200

    added validators

---
+++ src/validators/type_validator.py
@@ -0,0 +1,18 @@
+class TypeValidator:
+    """
+        Type Validator => validate data type
+    """
+    @staticmethod
+    def validate_str(value, context=""):
+        if not value or not isinstance(value, str):
+            raise TypeError(f"{context} error: {value} must be a string.")
+
+    @staticmethod
+    def validate_integer(value, context=""):
+        if not isinstance(value, int):
+            raise TypeError(f"{context} error: {value} must be an integer.")
+
+    @staticmethod
+    def validate_num(value, context=""):
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"{context} error: {value} must be a number.")
---
+++ src/validators/value_validator.py
@@ -0,0 +1,8 @@
+class ValueValidator:
+    """
+        Value Validator => validate data values
+    """
+    @staticmethod
+    def validate_non_negative(value, context=""):
+        if value < 0:
+            raise ValueError(f"{context} error: {value} can't be negative.")
